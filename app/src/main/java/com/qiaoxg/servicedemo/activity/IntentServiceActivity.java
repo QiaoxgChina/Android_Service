5561dce407afb7577d6cea6be234eb637015022d
qiaoxiaogang <qiaoxiaogang@zcgames.cn>
2017-08-23 15:15:39 +0800

    tiaIntentService

--- app/src/main/java/com/qiaoxg/servicedemo/activity/IntentServiceActivity.java
+++ app/src/main/java/com/qiaoxg/servicedemo/activity/IntentServiceActivity.java
@@ -1,15 +1,111 @@
 package com.qiaoxg.servicedemo.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.qiaoxg.servicedemo.R;
+import com.qiaoxg.servicedemo.service.IntentTypeService;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class IntentServiceActivity extends AppCompatActivity {
+
+    private static final String TAG = "IntentServiceActivity";
+
+    public static final String MY_SERVICE_ACTION = "MY_SERVICE_ACTION";
+    public static final String MY_THREAD_ACTION = "MY_THREAD_ACTION";
+
+    @BindView(R.id.serviceState_tv)
+    TextView serviceStateTv;
+    @BindView(R.id.threadState_tv)
+    TextView threadStateTv;
+    @BindView(R.id.progressBar)
+    ProgressBar progressBar;
+    @BindView(R.id.progressNumber_tv)
+    TextView progressNumberTv;
+    @BindView(R.id.startService_btn)
+    Button startServiceBtn;
+    @BindView(R.id.stopService_btn)
+    Button stopServiceBtn;
+
+
+    private LocalBroadcastManager mBroadcastManager;
+    private MyBroadcastReceiver mReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intent_service);
+        ButterKnife.bind(this);
+        mBroadcastManager = LocalBroadcastManager.getInstance(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //动态注册BroadcastReceiver，还有一种方式就是在manifest中静态注册
+        //本处是交给LocalBroadcastManager统一处理，也可以资金进行全局注册
+        //使用LocalBroadcastManager统一处理的，在收发broadcast的时候要保持一致，否则就会收不到消息等情况
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(MY_SERVICE_ACTION);
+        filter.addAction(MY_THREAD_ACTION);
+        mReceiver = new MyBroadcastReceiver();
+        mBroadcastManager.registerReceiver(mReceiver, filter);
+    }
+
+    @OnClick({R.id.startService_btn, R.id.stopService_btn})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.startService_btn:
+                startService(new Intent(this, IntentTypeService.class));
+                break;
+            case R.id.stopService_btn:
+                stopService(new Intent(this, IntentTypeService.class));
+                break;
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        //注销BroadcastReceiver
+        mBroadcastManager.unregisterReceiver(mReceiver);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    public class MyBroadcastReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            switch (intent.getAction()) {
+                case MY_SERVICE_ACTION:
+                    serviceStateTv.setText("服务状态: " + intent.getStringExtra("SERVICE_MSG"));
+                    break;
+                case MY_THREAD_ACTION:
+                    threadStateTv.setText("线程状态: " + intent.getStringExtra("THREAD_MSG"));
+                    int progress = intent.getIntExtra("THREAD_PROGRESS", 0);
+                    progressBar.setProgress(progress);
+                    progressNumberTv.setText(progress + "%");
+                    break;
+            }
+        }
     }
 }
--- app/src/main/java/com/qiaoxg/servicedemo/service/IntentTypeService.java
+++ app/src/main/java/com/qiaoxg/servicedemo/service/IntentTypeService.java
@@ -3,6 +3,11 @@ package com.qiaoxg.servicedemo.service;
 import android.app.IntentService;
 import android.content.Intent;
 import android.support.annotation.Nullable;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import static com.qiaoxg.servicedemo.activity.IntentServiceActivity.MY_SERVICE_ACTION;
+import static com.qiaoxg.servicedemo.activity.IntentServiceActivity.MY_THREAD_ACTION;
 
 /**
  * Created by admin on 2017/8/22.
@@ -10,12 +15,68 @@ import android.support.annotation.Nullable;
 
 public class IntentTypeService extends IntentService {
 
+    private static final String TAG = "IntentTypeService";
+
+    private LocalBroadcastManager mBroadcastManager;
+    private boolean isRunning = true;
+    private int count = 0;
+
     public IntentTypeService() {
         super("IntentTypeService");
     }
 
     @Override
-    protected void onHandleIntent(@Nullable Intent intent) {
+    public void onCreate() {
+        Log.e(TAG, "onCreate: 00000000000000000000");
+        super.onCreate();
+        mBroadcastManager = LocalBroadcastManager.getInstance(this);
+        sendServiceStateMsg("服务启动");
+    }
 
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        Log.e(TAG, "onHandleIntent: 2222222222222222222");
+        try {
+            sendThreadStateMsg("线程启动", count);
+            Thread.sleep(500);
+            sendServiceStateMsg("服务运行中...");
+            isRunning = true;
+            while (isRunning) {
+                count++;
+                sendThreadStateMsg("线程运行中...", count);
+                if (count >= 100) {
+                    isRunning = false;
+                }
+                Thread.sleep(100);
+                sendThreadStateMsg("线程运行中...", count);
+            }
+            sendThreadStateMsg("线程结束", count);
+
+        } catch (InterruptedException e) {
+            Log.e(TAG, "onHandleIntent: error is : " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.e(TAG, "onDestroy: 33333333333333333");
+        super.onDestroy();
+        sendServiceStateMsg("服务停止");
+    }
+
+    private void sendServiceStateMsg(String msg) {
+        Intent i = new Intent();
+        i.setAction(MY_SERVICE_ACTION);
+        i.putExtra("SERVICE_MSG", msg);
+        mBroadcastManager.sendBroadcast(i);
+    }
+
+    private void sendThreadStateMsg(String msg, int progress) {
+        Intent i = new Intent();
+        i.setAction(MY_THREAD_ACTION);
+        i.putExtra("THREAD_MSG", msg);
+        i.putExtra("THREAD_PROGRESS", progress);
+        mBroadcastManager.sendBroadcast(i);
     }
 }
